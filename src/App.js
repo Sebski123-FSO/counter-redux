d710a1c765c484f12974d0f04634e11a5f2d0827
sebski123 <sebthomle@gmail.com>
2022-02-23 22:11:06 +0100

    part 6 a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom";
+import React from "react";
+import { createStore } from "redux";
 
-function App() {
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      // if none of the above matches, code comes here
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={() => store.dispatch({ type: "INCREMENT" })}>
+        plus
+      </button>
+      <button onClick={() => store.dispatch({ type: "DECREMENT" })}>
+        minus
+      </button>
+      <button onClick={() => store.dispatch({ type: "ZERO" })}>
+        zero
+      </button>
     </div>
   );
-}
+};
+
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+renderApp();
+store.subscribe(renderApp);
 
 export default App;
